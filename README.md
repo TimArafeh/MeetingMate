67ab06548371cc8a42805902e17b37436babdf6a
Tim Arafeh <timarafeh2004@gmail.com>
2023-12-28 12:34:40 -0800

    Added Comments
    Just added comments all over the code explaining what each section does, also made the code a little easier to read by organizing everything a tiny bit. Also adjusted the README file so that it contains a full description of what the code does and how it works.

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # MeetingMate
 
-A basic Google Calendar Automation Tool that integrates with the Google Calendar API and Gmail API. This is a project as a part for a intership opportunity.
+This Python script is designed to integrate with the Google Calendar and Gmail APIs to automate the process of sending reminders for upcoming events. It features a user interface built with Tkinter, which allows users to select one-on-one meetings from their Google Calendar and send email reminders to the participants.
+
+Key Features:
+
+- Google API Integration: Utilizes the Google Calendar API to fetch upcoming events and the Gmail API to compose and send emails.
+- Authentication Handling: Manages OAuth2 authentication, storing credentials securely for easy access without constant reauthentication.
+- Email Composition: Generates email messages in MIME format, including event details such as summary, date, time, location, and description.
+- Event Retrieval: Obtains a list of the next ten upcoming events from the user's primary Google Calendar.
+- Timezone Conversion: Converts event times from UTC to Pacific Time (PT) in a human-readable AM/PM format.
+- Graphical User Interface (GUI): Provides a two-panel layout with a list box for selecting email addresses on the left and checkboxes for upcoming events on the right.
+- Batch Email Sending: Allows the user to select multiple events and send reminders to multiple email addresses in one operation.
+- Error Handling: Catches and prints out any HTTP errors that occur during API calls.
+
+How It Works:
+
+Authentication: On startup, the script checks for existing Google API credentials. If none are found or if they have expired, it initiates a login flow to authenticate the user and obtain the necessary tokens.
+
+Upcoming Events Fetching: Once authenticated, the script requests the upcoming events from the user's Google Calendar, filtering for one-on-one meetings.
+
+GUI Presentation: The script launches a Tkinter-based GUI, displaying a list of emails on the left side and a list of upcoming events on the right side. Users can select which emails to send reminders to and which events they pertain to.
+
+Email Reminder Composition: For each selected event, the script creates an email reminder with details of the event, formatted in a friendly message template.
+
+Email Sending: After the user confirms their selection, the script sends out the email reminders to the chosen recipients, using the Gmail API.
+
+Session End: Upon successful sending of reminders, the script informs the user and closes the GUI.
+
+Usage:
+
+This script is intended for program managers or individuals who need to manage event reminders efficiently. It requires a client secret file from Google API Console with appropriate permissions for the Google Calendar and Gmail APIs.
--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SCOPES = ["https://www.googleapis.com/auth/calendar",
-          "https://www.googleapis.com/auth/gmail.compose"]
-
 from datetime import datetime
 import pytz
 
@@ -20,7 +17,12 @@ import base64
 import tkinter as tk
 from tkinter import messagebox
 
+# Define the scopes required for Google Calender and Gmail API
+SCOPES = ["https://www.googleapis.com/auth/calendar",
+          "https://www.googleapis.com/auth/gmail.compose"]
+
 def create_message(sender, to, subject, event):
+    # Extract start and end times from the event
     start_time = event['start'].get('dateTime', event['start'].get('date'))
     end_time = event['end'].get('dateTime', event['end'].get('date'))
 
@@ -30,6 +32,7 @@ def create_message(sender, to, subject, event):
     if 'dateTime' in event['end']:
         end_time = convert_utc_to_ampm(end_time)
 
+    # Construct the email message text
     message_text = f"""
     Hello,
 
@@ -46,46 +49,55 @@ def create_message(sender, to, subject, event):
     Best regards,
     [Your Name]
     """
+
+    # Encode the message text in MIME format
     message = MIMEText(message_text)
     message['to'] = to
     message['from'] = sender
     message['subject'] = subject
     return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
+# Function to send an email message using the Gmail API
 def send_message(service, user_id, message):
     try:
+        # Send the message and print the message ID
         message = service.users().messages().send(userId=user_id, body=message).execute()
         print(f"Message Id: {message['id']}")
         return message
     except HttpError as error:
+        # Print the error if something goes wrong
         print(f"An error occurred: {error}")
         return None
 
+# Function to get a list of upcoming events from Google Calender API
 def get_upcoming_events(service, number_of_events=10):
+    # Get the current time in UTC format
     now = datetime.now(timezone.utc).isoformat()
+    # Request a list of upcoming events
     events_result = service.events().list(calendarId='primary', timeMin=now,
                                           maxResults=number_of_events, singleEvents=True,
                                           orderBy='startTime').execute()
     return events_result.get('items', [])
 
+# Function to convert UTC time string to a local time string in AM/PM format
 def convert_utc_to_ampm(utc_time_str):
-    print("Original UTC Time:", utc_time_str)  # Debug print
-
     # Parse the UTC time string with its timezone offset
     utc_time_with_tz = datetime.strptime(utc_time_str, '%Y-%m-%dT%H:%M:%S%z')
 
     # Convert to local time
     local_time = utc_time_with_tz.astimezone(pytz.timezone('America/Los_Angeles'))
-    print("Converted Local Time:", local_time.strftime('%Y-%m-%d %I:%M %p'))  # Debug print
 
     # Format the time in AM/PM format
     return local_time.strftime('%Y-%m-%d %I:%M %p')
 
 
+# Function to launch the graphical user interface
 def launch_gui(events, gmail_service):
+    # Initialize the root window
     root = tk.Tk()
     root.title("MeetingMate")
     root.geometry('1920x1080')
+    # Define a list of email addresses
     email_list = ['timarafeh2004@gmail.com', 'speedyslaytaker2004@gmail.com']
 
     # Frame for emails on the left
@@ -134,6 +146,7 @@ def launch_gui(events, gmail_service):
         chk.pack(anchor='nw', padx=10, pady=5)
         event_checkboxes[event['id']] = (var, event)
 
+    # Function to send created emails
     def on_send_emails():
         selected_indices = email_listbox.curselection()
         if not selected_indices:
@@ -157,33 +170,44 @@ def launch_gui(events, gmail_service):
 
     root.mainloop()
 
+# Define the function that will start the program
 def main():
     creds = None
     exit
+    # Check if the token.json file exists, which contains the user's access and refresh tokens
     if os.path.exists("token.json"):
+        # Load the credentials from the token.json file
         creds = Credentials.from_authorized_user_file("token.json")
     
+    # If there are no valid credentials available, let the user log in
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
+            # If the credentials are expired, refresh them
             creds.refresh(Request())
         else:
+            # If the credentials are not available, initiate the login
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
 
+        # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
+        # Build the service objects for the Google Calendar and Gmail APIs
         service = build("calendar", "v3", credentials=creds)
         gmail_service = build("gmail", "v1", credentials=creds)
 
+        # Get a list of upcoming events
         upcoming_events = get_upcoming_events(service, number_of_events=10)
 
         # Launch the GUI for selecting events to send emails
         launch_gui(upcoming_events, gmail_service)
 
+    # Catch any errors during the API calls
     except HttpError as error:
         print("An error occured: ", error)
 
+# Execute the main function when the script is run
 if __name__ == "__main__":
     main()
